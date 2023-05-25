0113bb8add552219f40020c4ee43546e0ce7ea98
Javier González <jgestarriol@gmail.com>
2023-05-25 23:13:59 +0200

    Add components

---
+++ src/components/Actions/actions.tsx
@@ -0,0 +1,13 @@
+import { Action } from "../callingButton/callingButton";
+import { Display } from "../display/display";
+
+export function Actions() {
+  return (
+    <>
+      <div className="actions">
+        <Action> </Action>
+        <Display></Display>
+      </div>
+    </>
+  );
+}
